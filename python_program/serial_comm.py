6b01f7120467adcd036ac1ff35d4a7dfe8ed1f9c
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2019-01-10 00:07:54 +0100

    ser+base

--- python_program/serial_comm.py
+++ python_program/serial_comm.py
@@ -9,16 +9,15 @@ import mysql.connector
 import serial
 from time import sleep
 
+
+
 ser = serial.Serial(port='COM3',baudrate=9600)
 
 
 
 def parser(string):
-    string = string[3:len(string)-3]
-    string = "("+string+")"
+    string = string.decode("utf-8") 
     return string
-
-
 
 def connect_to_db(db):
     mydb = mysql.connector.connect(
@@ -37,13 +36,15 @@ mydb=connect_to_db("bei_db")
 
 
 while(True):
+    val=""
     mycursor = mydb.cursor()
-    sql = "INSERT INTO `table1` (`id`, `tension`, `courant`, `temperature`) VALUES (%s,%s,%s,%s);"
+    sql = "INSERT INTO `table1` (`id`, `tension`, `courant`, `temperature`) VALUES "
     val=ser.readline()
     val=parser(val)
-    mycursor.execute(sql,val)
-    mydb.commit()
     print(val)
+    sql = sql + val + ";"
+    mycursor.execute(sql)
+    mydb.commit()
     sleep(3)
     
 
@@ -61,64 +62,3 @@ ser.close()
 
 
 
-
-
-"""
-def parser(string):
-    table = []
-    f_id=0
-    f_tension=0
-    f_courant=0
-    f_temperature=0
-    id=""
-    tension=""
-    courant=""
-    temperature=""
-    nbr_virgule=0
-    
-    for i in range(0,len(string)):
-        char = string[i]
-        if(char=="["):
-            # remplissage de id
-            f_id=1
-            f_tension=0
-            f_courant=0
-            f_temperature=0
-        elif(char==","):
-            nbr_virgule+=1
-            if(nbr_virgule==1):
-                f_tension=1
-                f_id=0
-                f_courant=0
-                f_temperature=0
-            elif(nbr_virgule==2):
-                f_tension=0
-                f_id=0
-                f_courant=1
-                f_temperature=0
-            elif(nbr_virgule==3):
-                f_tension=0
-                f_id=0
-                f_courant=0
-                f_temperature=1
-            else:
-                f_tension=0
-                f_id=0
-                f_courant=0
-                f_temperature=0    
-                tibari = 1
-        else:
-            f_tension=0
-            f_id=0
-            f_courant=0
-            f_temperature=0
-            tibari = 1
-        
-        if(f_id==1 || ):
-            id=string[i]
-                
-    
-    
-    
-    return table
-"""
